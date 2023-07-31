b79b80505d2271551f8e65b021652658cadc7b11
mateussc2000 <mateussc2000@gmail.com>
2023-07-31 20:55:44 -0300

    fix: cross origin bug

--- back-end/tvtime/src/main/java/com/java/tvtime/controller/MovieController.java
+++ back-end/tvtime/src/main/java/com/java/tvtime/controller/MovieController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.java.tvtime.model.Movie;
 import com.java.tvtime.service.MovieService;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
